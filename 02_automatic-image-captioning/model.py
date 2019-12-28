162ff5dcde3dbdf4a9fe149e71c8f47467725495
Miguel Martínez <miguelm@nvidia.com>
2019-12-28 22:22:37 +0000

    Solved!

--- 02_automatic-image-captioning/model.py
+++ 02_automatic-image-captioning/model.py
@@ -23,11 +23,56 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+                
+#        print("****")
+#        print("embed_size  :", embed_size)
+#        print("hidden_size :", hidden_size)
+#        print("vocab_size  :", vocab_size)
+#        print("num_layers  :", num_layers)
+#        print("****")
+                    
+        self.embed = nn.Embedding(num_embeddings=vocab_size,
+                                  embedding_dim=embed_size)
+#        print(self.embed)
+#        print("****")        
+                
+        self.lstm = nn.LSTM(input_size=embed_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True)        
+#        print(self.lstm)
+#        print("****")
+        
+        self.linear = nn.Linear(in_features=hidden_size,
+                                out_features=vocab_size)
+#        print(self.linear)
+#        print("****")
     
     def forward(self, features, captions):
-        pass
+#        print("----")
+#        print("features")
+#        print(features.shape)    
+#        print("----")
+#        print("captions")
+#        print(captions.shape)
+        
+        embeds = torch.cat((features.unsqueeze(1),self.embed(captions[:,:-1])), 1)
+        results = self.linear(self.lstm(embeds)[0])        
+        
+        return results
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+#        print(inputs.shape)
+        result = []
+        for index in range(max_len): 
+            out, states = self.lstm(inputs, states)
+            
+            out = out.squeeze(1)
+            out = self.linear(out)
+            res = out.max(1)[1]
+            result.append(res.item())
+            
+            inputs = self.embed(res).unsqueeze(1)
+        return result
